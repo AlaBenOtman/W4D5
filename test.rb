e11041ef8b72763d806990e9cc51852eaa5a9bd2
voldemort <alabenothman@gmail.com>
2021-03-19 13:07:05 -0700

    finished test.rb methods

--- test.rb
+++ test.rb
@@ -23,17 +23,17 @@ def my_min(arr)  O(n2)
 end
 
 def lc_subsum(list)
-    sum = 0
-    arr = []
-    list.each_index do |i|
-        tem = [list[i]]
-        (i+1...tem.length).each do |j|
-            tem << list[j]
-        end
-        arr << tem 
-    end
+    # sum = 0
+    # arr = []
+    # list.each_index do |i|
+    #     tem = [list[i]]
+    #     (i+1...tem.length).each do |j|
+    #         tem << list[j]
+    #     end
+    #     arr << tem 
+    # end
 
-    arr
+    # arr
     # arr.each do |ele|
     #     if ele[0] + ele[1] > sum 
     #         sum = ele[0] + ele[1]
@@ -41,17 +41,45 @@ def lc_subsum(list)
 
     # end
     # sum
+    require 'byebug'
+    def lc_subsum(list)
+        # debugger
+        sum = 0
+        arr =[] 
+        (0...list.length).each do |i|
+            tem = []
+            (i...list.length).each do |j|
+                tem << list[j]
+                arr << tem.dup
+            end
+        end
+        arr.each do |sub|
+            if sum < sub.sum 
+                sum = sub.sum 
+            end
+            
+        end
+        sum
+    end
 
 end
 
-def better_subsum(list)
+def better_subsum(list) #big(O(n))
     curr_sum = 0
     sum = 0
-    # (0...list.length-1).each do |i|
-    #     curr_sum = list[i] + list[i+1]
-    #     if curr_sum > sum 
-    #         sum = curr_sum
-    #     end
-    # end
+    i = 0
+    j =0
+    while i != list.length-1
+        curr_sum += list[j]
+        sum = curr_sum if curr_sum > sum 
+        if j == list.length-1
+            i+=1
+            j=i 
+            curr_sum = 0
+        else 
+            j+=1
+        end
+    end
     sum
 end
+
