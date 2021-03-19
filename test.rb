7c8811958dc5912d021b350ced78831da38b3b9e
voldemort <alabenothman@gmail.com>
2021-03-19 11:10:56 -0700

    created n^2 methods

--- test.rb
+++ test.rb
@@ -10,14 +10,33 @@ def butter_min(arr)
 end
 
 
-def my_min(arr)         
+def my_min(arr)  O(n2)        
     min = arr.first
     arr.each_with_index do |ele1, i|
        (i+1...arr.length).each do |ele2|
-            if arr[ele2] < ele1 
+            if arr[ele2] < ele1 && arr[ele2] < min
                 min = arr[ele2]
             end
         end
     end
     min
+end
+
+def lc_subsum(list)
+    sum = 0
+    arr = []
+    list.each_index do |i|
+        (i+1...list.length).each do |j|
+            if j != i 
+                arr << [list[i],list[j]]
+            end
+        end
+    end
+    arr.each do |ele|
+        if ele[0] + ele[1] > sum 
+            sum = ele[0] + ele[1]
+        end
+
+    end
+    sum
 end
