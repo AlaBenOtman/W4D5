838b8d2fa93adf020b30929c2687df46a694758a
ShenQi1996 <qisam1989@gmail.com>
2021-03-19 13:49:43 -0400

    butter_min is done

--- test.rb
+++ test.rb
@@ -1,4 +1,23 @@
-class Test 
-    
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+def butter_min(arr)
+    min = arr.first
+    arr.each do |ele|      #O(N)
+        if ele < min
+            min = ele
+        end
+    end
+    min
+end
 
+
+def my_min(arr)         
+    min = arr.first
+    arr.each_with_index do |ele1, i|
+       (i+1...arr.length).each do |ele2|
+            if arr[ele2] < ele1 
+                min = arr[ele2]
+            end
+        end
+    end
+    min
 end
